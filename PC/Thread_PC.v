1f737e4bf4ed87ee41ef51aafd650fb9335382fe
Ling <github@ling.moe>
2025-03-12 13:17:26 -0700

    finish the reset logic for thread supported PC

--- PC/Thread_PC.v
+++ PC/Thread_PC.v
@@ -12,68 +12,110 @@ module Thread_PC(
 );
 
     reg [63:0] PC1, PC2, PC3, PC4;
-    reg PC_ctrl1, PC_ctrl2, PC_ctrl3, PC_ctrl4;
+    reg PC_ctrl_1, PC_ctrl_2, PC_ctrl_3, PC_ctrl_4;
     reg [63:0] BTA1, BTA2, BTA3, BTA4;
-    reg [63:0] PC_next1, PC_next2, PC_next3, PC_next4;
-    reg [63:0] PC_next_to_mux1, PC_next_to_mux2, PC_next_to_mux3, PC_next_to_mux4;
-    reg Counter_Enable1, Counter_Enable2, Counter_Enable3, Counter_Enable4;
+    reg [63:0] PC_next_1, PC_next_2, PC_next_3, PC_next_4;
+    reg [63:0] PC_next_to_mux_1, PC_next_to_mux_2, PC_next_to_mux_3, PC_next_to_mux_4;
+    reg Counter_Enable_1, Counter_Enable_2, Counter_Enable_3, Counter_Enable_4;
+    reg rst_1, rst_2, rst_3, rst_4;
 
-    PC PC1(.clk(clk),.rst(rst),.PC_next(PC_next1),.PC(PC1));
-    PC PC2(.clk(clk),.rst(rst),.PC_next(PC_next2),.PC(PC2));
-    PC PC3(.clk(clk),.rst(rst),.PC_next(PC_next3),.PC(PC3));
-    PC PC4(.clk(clk),.rst(rst),.PC_next(PC_next4),.PC(PC4));
+    PC PC1(.clk(clk),.rst(rst),.PC_next(PC_next_1),.PC(PC1));
+    PC PC2(.clk(clk),.rst(rst),.PC_next(PC_next_2),.PC(PC2));
+    PC PC3(.clk(clk),.rst(rst),.PC_next(PC_next_3),.PC(PC3));
+    PC PC4(.clk(clk),.rst(rst),.PC_next(PC_next_4),.PC(PC4));
 
-    PC_plus_1 PC_plus_1_1(.PC(PC1), .enable(Counter_Enable1), .PC_next(PC_next_to_mux1));
-    PC_plus_1 PC_plus_1_2(.PC(PC2), .enable(Counter_Enable2), .PC_next(PC_next_to_mux2));
-    PC_plus_1 PC_plus_1_3(.PC(PC3), .enable(Counter_Enable3), .PC_next(PC_next_to_mux3));
-    PC_plus_1 PC_plus_1_4(.PC(PC4), .enable(Counter_Enable4), .PC_next(PC_next_to_mux4));
+    PC_plus_1 PC_plus_1_1(.PC(PC1), .enable(Counter_Enable_1), .PC_next(PC_next_to_mux_1));
+    PC_plus_1 PC_plus_1_2(.PC(PC2), .enable(Counter_Enable_2), .PC_next(PC_next_to_mux_2));
+    PC_plus_1 PC_plus_1_3(.PC(PC3), .enable(Counter_Enable_3), .PC_next(PC_next_to_mux_3));
+    PC_plus_1 PC_plus_1_4(.PC(PC4), .enable(Counter_Enable_4), .PC_next(PC_next_to_mux_4));
 
-    PC_MUX PC_MUX_1 (.BTA(BTA1), .PC_ctrl(PC_ctrl1), .PC_next_in(PC_next_to_mux1), .PC_next_out(PC_next1));
-    PC_MUX PC_MUX_2 (.BTA(BTA2), .PC_ctrl(PC_ctrl2), .PC_next_in(PC_next_to_mux2), .PC_next_out(PC_next2));
-    PC_MUX PC_MUX_3 (.BTA(BTA3), .PC_ctrl(PC_ctrl3), .PC_next_in(PC_next_to_mux3), .PC_next_out(PC_next3));
-    PC_MUX PC_MUX_4 (.BTA(BTA4), .PC_ctrl(PC_ctrl4), .PC_next_in(PC_next_to_mux4), .PC_next_out(PC_next4));
+    PC_MUX PC_MUX_1 (.BTA(BTA1), .PC_ctrl(PC_ctrl_1), .PC_next_in(PC_next_to_mux_1), .PC_next_out(PC_next_1));
+    PC_MUX PC_MUX_2 (.BTA(BTA2), .PC_ctrl(PC_ctrl_2), .PC_next_in(PC_next_to_mux_2), .PC_next_out(PC_next_2));
+    PC_MUX PC_MUX_3 (.BTA(BTA3), .PC_ctrl(PC_ctrl_3), .PC_next_in(PC_next_to_mux_3), .PC_next_out(PC_next_3));
+    PC_MUX PC_MUX_4 (.BTA(BTA4), .PC_ctrl(PC_ctrl_4), .PC_next_in(PC_next_to_mux_4), .PC_next_out(PC_next_4));
 
     always @(*) begin
-        case(thread)
-            2'b00: begin
-                PC_out = PC1;
-                PC_ctrl4 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
-                BTA4 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
-
-                Counter_Enable1 = 1'b1;
-                Counter_Enable2 = 1'b0;
-                Counter_Enable3 = 1'b0;
-                Counter_Enable4 = 1'b0;
+        case(rst)
+            1'b1: begin
+                // reset each PC individually
+                case(thread)
+                    2'b00: begin
+                        rst_1 = 1'b1;
+                        rst_2 = 1'b0;
+                        rst_3 = 1'b0;
+                        rst_4 = 1'b0;
+                    end
+                    2'b01: begin
+                        rst_1 = 1'b0;
+                        rst_2 = 1'b1;
+                        rst_3 = 1'b0;
+                        rst_4 = 1'b0;
+                    end
+                    2'b10: begin
+                        rst_1 = 1'b0;
+                        rst_2 = 1'b0;
+                        rst_3 = 1'b1;
+                        rst_4 = 1'b0;
+                    end
+                    2'b11: begin
+                        rst_1 = 1'b0;
+                        rst_2 = 1'b0;
+                        rst_3 = 1'b0;
+                        rst_4 = 1'b1;
+                    end
+                endcase
             end
-            2'b01: begin
-                PC_out = PC2;
-                PC_ctrl1 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
-                BTA1 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+            1'b0: begin
 
-                Counter_Enable1 = 1'b0;
-                Counter_Enable2 = 1'b1;
-                Counter_Enable3 = 1'b0;
-                Counter_Enable4 = 1'b0;
-            end
-            2'b10: begin
-                PC_out = PC3;
-                PC_ctrl2 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
-                BTA2 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+                // reset signal all clear
+                rst_1 = 1'b0;
+                rst_2 = 1'b0;
+                rst_3 = 1'b0;
+                rst_4 = 1'b0;
 
-                Counter_Enable1 = 1'b0;
-                Counter_Enable2 = 1'b0;
-                Counter_Enable3 = 1'b1;
-                Counter_Enable4 = 1'b0;
-            end
-            2'b11: begin
-                PC_out = PC4;
-                PC_ctrl3 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
-                BTA3 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+                case(thread)
+                    2'b00: begin
+                        PC_out = PC1;
+                        PC_ctrl_4 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
+                        BTA4 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
 
-                Counter_Enable1 = 1'b0;
-                Counter_Enable2 = 1'b0;
-                Counter_Enable3 = 1'b0;
-                Counter_Enable4 = 1'b1;
+                        Counter_Enable_1 = 1'b1;
+                        Counter_Enable_2 = 1'b0;
+                        Counter_Enable_3 = 1'b0;
+                        Counter_Enable_4 = 1'b0;
+                
+                    end
+                    2'b01: begin
+                        PC_out = PC2;
+                        PC_ctrl_1 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
+                        BTA1 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+
+                        Counter_Enable_1 = 1'b0;
+                        Counter_Enable_2 = 1'b1;
+                        Counter_Enable_3 = 1'b0;
+                        Counter_Enable_4 = 1'b0;
+                    end
+                    2'b10: begin
+                        PC_out = PC3;
+                        PC_ctrl_2 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
+                        BTA2 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+
+                        Counter_Enable_1 = 1'b0;
+                        Counter_Enable_2 = 1'b0;
+                        Counter_Enable_3 = 1'b1;
+                        Counter_Enable_4 = 1'b0;
+                    end
+                    2'b11: begin
+                        PC_out = PC4;
+                        PC_ctrl_3 = PC_ctrl_in;  // Control signal is given by Branch Decision Unit in ID stage, hence it is one cycle behind
+                        BTA3 = BTA_in;  // BTA_in is given by Offset Extend Unit in ID stage, hence it is one cycle behind
+
+                        Counter_Enable_1 = 1'b0;
+                        Counter_Enable_2 = 1'b0;
+                        Counter_Enable_3 = 1'b0;
+                        Counter_Enable_4 = 1'b1;
+                    end
+                endcase
             end
         endcase
     end
